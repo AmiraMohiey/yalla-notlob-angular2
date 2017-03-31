360eb438620f188861f187d252614d519c93125c
AmiraMohiey <ameramohiey92@gmail.com>
2017-03-31 14:48:37 +0200

    signup,login forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,9 +7,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  error="";
+  user={
+  name:"",
+  email:"",
+  username:"",
+  password:""
+       }
+confirm="";
 onSubmit(form: NgForm){
-  console.log(form);
+  console.log(this.user);
+  if(this.user.password!=this.confirm){this.error="passwords don't match"}
+  else{this.error="";}
 
 }
 
