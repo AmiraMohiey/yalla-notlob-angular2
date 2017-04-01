17a73f6a217fd5fba9e66db8ca1c2602431bcea5
mhesham90 <mohammedhesham.1990@gmail.com>
2017-04-01 04:10:37 +0200

    sending test http request from signup to firebase

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Headers, Http } from '@angular/http';
 
 @Component({
   selector: 'app-signup',
@@ -7,24 +8,39 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  error="";
-  user={
-  name:"",
-  email:"",
-  username:"",
-  password:""
-       }
-confirm="";
-onSubmit(form: NgForm){
-  console.log(this.user);
-  if(this.user.password!=this.confirm){this.error="passwords don't match"}
-  else{this.error="";}
+  error = '';
+  user = {
+  name: '',
+  email: '',
+  username: '',
+  password: ''
+       };
+  confirm = '';
 
-}
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
+  sendRegData() {
+    const body = JSON.stringify(this.user);
+    const headers = new Headers({
+      'Content-Type': 'application/json'
+    });
+    this.http.post('https://notlob-2eee7.firebaseio.com/user.json', body, {headers: headers}).subscribe(
+      data => console.log(data)
+    );
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(this.user);
+    if (this.user.password !== this.confirm) {
+      this.error = 'passwords don\'t match';
+    } else {
+      this.sendRegData();
+      this.error = '' ;
+    }
+
+  }
 }
