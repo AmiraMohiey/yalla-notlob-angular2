11cbb883dd961d93192d1a309a543c6f787778e2
AmiraMohiey <ameramohiey92@gmail.com>
2017-04-03 06:24:07 +0200

    templates except groups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,25 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { routing} from './app.routing';
 import { AppService } from './app.service';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
+import { AddorderComponent } from './addorder/addorder.component';
+import { OrdersComponent } from './orders/orders.component';
+import { VieworderComponent } from './vieworder/vieworder.component';
+import { GroupsComponent } from './groups/groups.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    FriendsComponent,
+    AddorderComponent,
+    OrdersComponent,
+    VieworderComponent,
+    GroupsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,27 @@
 import{Routes,RouterModule} from "@angular/router";
 import{LoginComponent} from "./login/login.component";
 import{SignupComponent} from "./signup/signup.component";
+import{HomeComponent} from "./home/home.component";
+import{FriendsComponent} from "./friends/friends.component";
+import{AddorderComponent} from "./addorder/addorder.component";
+import{OrdersComponent} from "./orders/orders.component";
+import{VieworderComponent} from "./vieworder/vieworder.component";
+import{GroupsComponent} from "./groups/groups.component";
 const APP_ROUTES: Routes=[
 
 {path: 'login',component:LoginComponent},
 {path: 'signup',component:SignupComponent}
-
+,
+{path: 'home',component:HomeComponent}
+,
+{path: 'friends',component:FriendsComponent}
+,
+{path: 'addorder',component:AddorderComponent}
+,
+{path: 'orders',component:OrdersComponent}
+,
+{path: 'vieworder',component:VieworderComponent},
+{path: 'groups',component:GroupsComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
