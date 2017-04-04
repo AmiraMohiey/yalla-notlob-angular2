aef88efb03a42208f6cd857e02a160d4904af7f6
AmiraMohiey <ameramohiey92@gmail.com>
2017-04-04 17:33:01 +0200

    friends component

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-friends=[{"name":"amira","avatar":"../assets/u.png"},{"name":"salma","avatar":"../assets/u.png"}]
-  constructor() { }
+  friendemail="";
+  error="";
+  friends=[{"name":"amira","avatar":"../assets/u.png"},
+           {"name":"salma","avatar":"../assets/u.png"}]
+  constructor() {}
+  ngOnInit() {}
+   onSubmit(form: NgForm) {
+    console.log(this.friendemail);
+  //if no username { error="sorry no such user"}
 
-  ngOnInit() {
   }
+
 
 }
