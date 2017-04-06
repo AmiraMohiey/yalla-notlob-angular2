5bf5bcb5c61ae294562601d5018b5963d727827f
mhesham90 <mohammedhesham.1990@gmail.com>
2017-04-06 15:17:06 +0200

    fixing friends component conflict

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -9,29 +9,11 @@ import { FriendsService } from './friends.service';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-<<<<<<< HEAD
-  friendemail="";
-  newfriend={name:"",avatar:""}
-  error="";
-  friends=[{"name":"amira","avatar":"../assets/u.png"},
-           {"name":"salma","avatar":"../assets/u.png"}]
-  constructor() {}
-  ngOnInit() {}
-   onSubmit(form: NgForm) {
-    console.log(this.friendemail);
- 
-  if(this.friendemail!="")
- { this.newfriend.name=this.friendemail; 
-   this.newfriend.avatar="../assets/u.png" 
-this.error="";}
- else {this.error="no such user"
-}
-  
-=======
+
   friendemail= '';
+  newfriend= {name: '', avatar: ''};
   error= '';
-  friends= [{'name': 'amira', 'avatar': '../assets/u.png'},
-           {'name': 'salma', 'avatar': '../assets/u.png'}];
+  friends= [];
 
   constructor(private friendsService: FriendsService) {}
 
@@ -39,7 +21,7 @@ this.error="";}
     this.friendsService.getFriends().subscribe(
       data => {
         const keyArr = [];
-        for (let key in data) {
+        for (const key in data) {
           keyArr.push(data[key]);
         }
         this.friends = keyArr;
@@ -54,7 +36,6 @@ this.error="";}
       data => console.log(data)
     );
   // if no username { error="sorry no such user"}
->>>>>>> aba1f46707e8017c14296f616bacc2245bb01424
 
   }
 
